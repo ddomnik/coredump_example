d44eacec2b0c15fca6cabb93c6670271e5030a64
Dominik DUFNER <dominik.dufner@alten.com>
2022-08-26 08:29:10 +0200

    cleanup

--- main/main.c
+++ main/main.c
@@ -3,14 +3,12 @@
 #include <string.h>
 #include <esp_log.h>
 #include "esp_partition.h"
-
-
-/*******************************************************/
-/***************** Local Macros ************************/
-/*******************************************************/
+#include "esp_core_dump.h"
 
 #define BUFFER_SIZE 4096
 #define PARTITION_NAME "coredump"
+
+const char TAG[] = "CoreDump:";
 
 
 /* SDK Configuration
@@ -30,9 +28,9 @@
  *
  */
 
-void PMA_Main();
-const esp_partition_t* findPMACoreDumpPartition();
-void readPMACoreDump(const esp_partition_t* part_CoreDump, char* content, long offset, long size);
+void CoreDump_Main();
+const esp_partition_t* findCoreDumpPartition();
+void readCoreDump(const esp_partition_t* part_CoreDump, char* content, long offset, long size);
 
 COREDUMP_DRAM_ATTR uint8_t global_var;
 
@@ -47,7 +45,7 @@ void app_main(void)
 {
 	ESP_LOGI("X","START");
 
-	PMA_Main();
+	CoreDump_Main();
 
     global_var = 25;
 
@@ -63,42 +61,57 @@ void app_main(void)
 
 
 
-void PMA_Main()
+void CoreDump_Main()
 {
-    const esp_partition_t* partition = findPMACoreDumpPartition();
+    size_t out_cd_addr;
+    size_t out_cd_size;
+
+    esp_core_dump_image_get(&out_cd_addr, &out_cd_size);
+    ESP_LOGI(TAG, "core dump addr: %d", out_cd_addr);
+    ESP_LOGI(TAG, "core dump size: %d", out_cd_size);
+
+
+    const esp_partition_t* partition = findCoreDumpPartition();
 
     if(NULL != partition)
     {
-        ESP_LOGI("PMA", "PMA partition found with size: %d", partition->size);
+        ESP_LOGI(TAG, "partition found with size: %d", partition->size);
 
-        static char buffer[BUFFER_SIZE];
-
-        readPMACoreDump(partition, buffer, 0, BUFFER_SIZE);
-
-        for(int i = 0; i < partition->size; i += BUFFER_SIZE)
+        if(out_cd_size > 0)
         {
-            memset(buffer, 0, BUFFER_SIZE);
-            readPMACoreDump(partition, buffer, i, BUFFER_SIZE);
+			static char buffer[BUFFER_SIZE];
 
-            ESP_LOGI("PMA", "OFFSET: %d \n", i);
+			readCoreDump(partition, buffer, 0, BUFFER_SIZE);
 
-            for (int n = 0 ; n < BUFFER_SIZE; n++ )
-            {
-               printf("%02x", buffer[n]);
-            }
-            printf("\n");
+			for(int i = 0; i < BUFFER_SIZE*2; i += BUFFER_SIZE)
+			{
+				memset(buffer, 0, BUFFER_SIZE);
+				readCoreDump(partition, buffer, i, BUFFER_SIZE);
 
+				ESP_LOGI(TAG, "OFFSET: %d \n", i);
+
+				for (int n = 0 ; n < BUFFER_SIZE; n++ )
+				{
+				   printf("%02x", buffer[n]);
+				}
+				printf("\n");
+
+			}
+        }
+        else
+        {
+        	ESP_LOGI(TAG, "partition found with size: %d", partition->size);
         }
 
     }
     else
     {
-        ESP_LOGW("PMA", "PMA partition not found");
+        ESP_LOGW(TAG, "partition not found");
     }
 
 }
 
-const esp_partition_t* findPMACoreDumpPartition()
+const esp_partition_t* findCoreDumpPartition()
 {
     esp_partition_type_t p_type = ESP_PARTITION_TYPE_DATA;
     esp_partition_subtype_t p_subtype = ESP_PARTITION_SUBTYPE_DATA_COREDUMP;
@@ -107,9 +120,8 @@ const esp_partition_t* findPMACoreDumpPartition()
     return esp_partition_find_first(p_type, p_subtype, label);
 }
 
-void readPMACoreDump(const esp_partition_t* part_CoreDump, char* content, long offset, long size)
+void readCoreDump(const esp_partition_t* part_CoreDump, char* content, long offset, long size)
 {
-  esp_err_t func_ret;
-  func_ret = esp_partition_read(part_CoreDump, offset, content, size);
+	esp_partition_read(part_CoreDump, offset, content, size);
 }
 
